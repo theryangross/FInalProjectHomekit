a0f046deb7936719233f11c50086671fee13f03c
Ryan Gross <ryangross123@icloud.com>
2019-05-08 00:50:02 -0500

    won't display data

--- Pages/Products/Index.cshtml.cs
+++ Pages/Products/Index.cshtml.cs
@@ -14,129 +14,62 @@ namespace FInalProjectHomekit.Pages.Products
     public class IndexModel : PageModel
     {
         private readonly FInalProjectHomekit.Models.HomekitDbContext _context;
-        private readonly ILogger _logger;
 
-        public IndexModel(FInalProjectHomekit.Models.HomekitDbContext context, ILogger<IndexModel> logger)
+        public IndexModel(FInalProjectHomekit.Models.HomekitDbContext context)
         {
-            _logger = logger;
             _context = context;
         }
 
         public IList<Product> Product { get;set; }
 
         [BindProperty(SupportsGet = true)]
-        public string SearchString { get; set; }
-        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
-        public SelectList Categories { get; set; }
-        public SelectList SortList {get; set;}
-        [BindProperty(SupportsGet = true)]
-        public string ProductCategory { get; set; }
-
-        // PageSize: How many records to display per page.
-        // Default: 10
-        public int PageSize {get; set;} = 10;
-
-        // PageNum: Current Page Number we are on. Default is 1
-        [BindProperty(SupportsGet = true)]
         public int PageNum {get; set;} = 1;
+        public int PageSize {get; set;} = 5;
 
-        public int TotalCount {get; set;} // Total number of records
-        public int TotalPages {get; set;} // Total number of pages
-        
-        // String to keep track of our current sort order. Needed for 
-        // paging to remember current sort
-        public string CurrentSort {get; set;} 
-        
-        // Next Sort for ProductName and BrandName sort. This alternates between asc/desc
-        public string ProductNameSort {get; set;} = "ProductName_asc";
-        public string PriceSort {get; set;} = "Price_asc";
+        [BindProperty(SupportsGet = true)]
+        public string CurrentSort {get; set;}
+        public SelectList SortList {get; set;}
+        public string CategoryName {get; set;}
 
-        public async Task OnGetAsync(string sortOrder)
+        public async Task OnGetAsync()
         {
-            CurrentSort = sortOrder;
+            var products = _context.Product.Include(r => r.Brand).Include(r => r.Category).Select(r => r);
 
-            // Sorting Technique 2. Create a SelectList of sort options.
-            // Set the values equal to our expected sort strings
+            var query = _context.Product.Select(r => r);
             List<SelectListItem> sortItems = new List<SelectListItem> {
-                new SelectListItem {Text = "Product Name A - Z", Value = "ProductName_asc"},
-                new SelectListItem("Product Name Z - A", "ProductName_desc"),
-                new SelectListItem("Price Low - High", "Price_asc"),
-                new SelectListItem("Price High - Loq", "Price_desc")
+                new SelectListItem { Text = "Product Name Ascending", Value = "PN_asc" },
+                new SelectListItem { Text = "Product Name Descending", Value = "PN_desc" },
+                new SelectListItem { Text = "Category Name Ascending", Value = "CN_asc" },
+                new SelectListItem { Text = "Category Name Descending", Value = "CN_desc" },
+                new SelectListItem { Text = "Price Ascending", Value = "Price_asc" },
+                new SelectListItem { Text = "Price Descending", Value = "Price_desc" }
             };
-            // Default value will be CurrentSort
             SortList = new SelectList(sortItems, "Value", "Text", CurrentSort);
-            
-            // Use LINQ to get list of categories.
-            IQueryable<string> categoryQuery = from m in _context.Product
-                                            orderby m.CategoryName
-                                            select m.CategoryName;
 
-            //var movies = from m in _context.Movie
-            //            select m;
-            // var movies = _context.Movie.Include(m => m.Reviews).Select(m => new {
-            //     ID = m.ID,
-            //     Title = m.Title,
-            //     ReleaseDate = m.ReleaseDate,
-            //     Genre = m.Genre,
-            //     Price = m.Price,
-            //     Rating = m.Rating,
-            //     Review = m.Reviews.Average(r => r.Score)            
-            // });
-            // IQueryable<Movie> movies = _context.Movie.Include(m => m.Reviews);
-            // var movies = (from m in _context.Movie
-            //     select m).Include("Reviews");
-            
-            // Use .Include() to bring in the reviews
-            var products = _context.Product.Select(m => m);
-            
-            if (!string.IsNullOrEmpty(SearchString))
+            switch (CurrentSort)
             {
-                products = products.Where(s => s.ProductName.Contains(SearchString));
-            }
-            
-            if (!string.IsNullOrEmpty(ProductCategory))
-            {
-                products = products.Where(x => x.CategoryName == ProductCategory);
-            }
-
-            // Calculate total number of records and how many pages that takes
-            TotalCount = products.Count();
-            TotalPages = (int)Math.Ceiling(TotalCount / (double)PageSize);
-
-            Categories = new SelectList(await categoryQuery.Distinct().ToListAsync());
-
-            // Do the sorting. This is the brains of sorting
-            // Switch on sort Order and perform proper LINQ query
-            switch (sortOrder) 
-            {
-                case "ProductName_desc":
-                    _logger.LogInformation("Sorting by Title descending");
-                    products = products.OrderByDescending(m => m.ProductName);
-                    ProductNameSort = "title_asc";
+                case "PN_asc":
+                    query = query.OrderBy(r => r.ProductName);
                     break;
-                case "ProductName_asc":
-                    _logger.LogInformation("Sorting by Title ascending");
-                    products = products.OrderBy(m => m.ProductName);
-                    ProductNameSort = "title_desc";
+                case "PN_desc":
+                    query = query.OrderByDescending(r => r.ProductName);
                     break;
-                case "date_desc":
-                    _logger.LogInformation("Sorting by Release Date descending");
-                    products = products.OrderByDescending(m => m.ProductPrice);
-                    PriceSort = "date_asc";
+                case "CN_asc":
+                    query = query.OrderBy(r => r.CategoryName);
                     break;
-                case "date_asc":
-                    _logger.LogInformation("Sorting by Release Date ascending");
-                    products = products.OrderBy(m => m.ProductPrice);
-                    PriceSort = "date_desc";
+                case "CN_desc":
+                    query = query.OrderByDescending(r => r.CategoryName);
                     break;
-                default:
-                    _logger.LogInformation("No Sorting");
+                case "Price_asc":
+                    query = query.OrderBy(r => r.ProductPrice);
+                    break;
+                case "Price_desc":
+                    query = query.OrderByDescending(r => r.ProductPrice);
                     break;
             }
+            Product = await products.ToListAsync();
 
-            // Pick the movies for paging. Skip() to current page and then Take the right
-            // number of movies
-            Product = await products.Skip((PageNum - 1) * PageSize).Take(PageSize).ToListAsync();
+            Product = await query.Skip((PageNum-1)*PageSize).Take(PageSize).ToListAsync();
         }
     }
 }
